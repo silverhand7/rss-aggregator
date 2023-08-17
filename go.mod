5e06f86150edad10bd6e84e2c23986308e1a9f01
silverhand7 <refojunior5@gmail.com>
2023-08-17 12:21:20 +0800

    build own server with json response

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/silverhand7/go-rss-aggregator
+
+go 1.19
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
---
+++ json.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON Response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error:", message)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: message,
+	})
+}
